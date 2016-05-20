06268cee9ce70e0a92d269a26d4ecdec7fac10b5
kosieram@stolaf.edu <kosieram@stolaf.edu>
2016-05-20 17:39:26 -0500

    wip

--- Translator.py
+++ Translator.py
@@ -207,12 +207,10 @@ f=c+e
 """
 
 expr2= """
-a=1
-b=2
-a<b
+print("hello world")
 """
 
 tree = ast.parse(expr)
 print(ast.dump(tree))
 Translator().translate(tree)
-print(ast.dump(ast.parse(expr2)))
+#print(ast.dump(ast.parse(expr2)))
