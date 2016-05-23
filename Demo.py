7ad2570b539ed6309bda8822e105c8986c8d77cd
knightflight377 <emholm12@gmail.com>
2016-05-22 21:39:12 -0500

    Removed comments and fibonacci function

--- Demo.py
+++ Demo.py
@@ -1,16 +1,8 @@
-#This is a demo Python program that uses as many implemented features as possible
+def printNumbers():
+	a = 0
+	while a < 50:
+		print(a)
 
-#Prints all of the Fibonacci numbers that are less than b
-def fibonacci():
-	a, b = 0, 1
-	while b < 200:
-		print(b)
-		a, b = b, a+b
-
-#Prints the amount of money a graduate will pay per month
-#loan is the principal loan amount
-#interest is the annual interest rate, and we're assuming that you make a payment every month
-#months is the number of months you're paying for
 def studentLoans(loan, interest, months):
 	principal = loan
 	rate = interest/12
@@ -20,7 +12,6 @@ def studentLoans(loan, interest, months):
 	payment = principal*(numerator/denominator)
 	print("You will pay:", payment, "dollars per month")
 
-#Determines if n in the given range is prime
 def prime():
         for n in range(2, 13):
                 for x in range(2, n):
@@ -30,14 +21,14 @@ def prime():
                         else:
                                 print(n, "is a prime number")
 
-#A basic, cute hello world program
+
 def helloWorld(string):
         introduction = "Hello World! My name is "
         name = string
         greeting = introduction + name
         print(greeting)
 
-#Average function
+
 def average(n1, n2, n3):
         if not(n1 == n2 == n3 == 0):
                 average = (n1 + n2 + n3)/3
@@ -45,8 +36,8 @@ def average(n1, n2, n3):
         else:
                 print("The average is 0")
 
-print("Fibonacci Function")
-fibonacci()
+print("Print some Numbers")
+printNumbers()
 print("\n")
 
 print("Student Loan Calculator Function")
