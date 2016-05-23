c2e95abb356feca6ac15ff9cbed135f373b6b10b
knightflight377 <emholm12@gmail.com>
2016-05-22 22:44:54 -0500

    Changed demo file slightly

--- Demo.py
+++ Demo.py
@@ -31,9 +31,10 @@ def helloWorld(string):
 
 def average(n1, n2, n3):
         if n1 > 0 and n2 > 0 and n3 >0:
-                sum = n1 + n2 + n3
-                average = sum/3
-                print("The average is", average)
+                summ = n1 + n2 + n3
+                ave = summ/3
+                print("The average is ")
+                print(ave)
         else:
                 print("The average is 0")
 
