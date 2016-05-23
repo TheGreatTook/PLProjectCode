0cb76cad35d285a56a2f94658a80a48e30da5f27
knightflight377 <emholm12@gmail.com>
2016-05-22 21:56:11 -0500

    Changed average function in demo

--- Demo.py
+++ Demo.py
@@ -30,8 +30,9 @@ def helloWorld(string):
 
 
 def average(n1, n2, n3):
-        if not(n1 == n2 == n3 == 0):
-                average = (n1 + n2 + n3)/3
+        if n1 > 0 and n2 > 0 and n3 >0:
+                sum = n1 + n2 + n3
+                average = sum/3
                 print("The average is", average)
         else:
                 print("The average is 0")
